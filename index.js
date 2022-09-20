a7b70b246f36201c9f9be6bffc6d6f3661b3b000
Grace Durant <gdurant34@gmail.com>
2022-09-20 16:41:02 -0700

    commit tree a60cf3628dd95c5585b60ebe6fccf0ba8195e359

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const targetedElement = document.getElementById('text');
+    targetedElement.textContent = "This is really cool!";
+  });
